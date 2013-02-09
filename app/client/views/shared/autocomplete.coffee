af8b6c6cedf1cb90e314d5f98015656ca5a62307
Craig Spaeth <craigspaeth@gmail.com>
2013-02-09 17:52:29 -0500

    home-grown-autocomplete

--- app/client/views/shared/autocomplete.coffee
+++ app/client/views/shared/autocomplete.coffee
@@ -1,22 +1,49 @@
 class window.AutocompleteView extends Backbone.View
   
+  defaults:
+    field: 'name'
+    delay: 300
+    
   initialize: ->
-    @search = _.debounce @search, 100
-    @$results = @$el.after("<div class='results'></div>").next('.results')
-    
-  events:
-    'keyup': 'search'
-    
+    @options = _.extend @defaults, @options
+    @search = _.debounce @search, @options.delay
+    @$results = @$el.after("<div class='ac_results'></div>").next('.ac_results')
+
+  renderLi: (obj) ->
+    "<li>#{obj[@options.field]}</li>"
+  
+  renderResults: (arr) ->
+    "<ul>#{(@renderLi(obj) for obj in arr).join('')}</ul>"
+  
   search: =>
     $.ajax(
       url: @options.url
       data: term: @$el.val()
-    ).then (res) =>
-      @$results.html @resToHTML res
-      
-  resToHTML: (res) =>
-    """
-      <ul>
-        #{("<li>#{obj.name}</li>" for obj in res).join('')}
-      </ul>
-    """
+    ).then (@res) =>
+      @$results.html @renderResults @res
+      @$results.find('li').click @select
+    
+  over: (dir) ->
+    $over = @$results.find('.ac_over')
+    $over.parent().children().removeClass('ac_over')
+    $next = $over[dir]()
+    unless $next.length
+      @$results.find('li:first').addClass('ac_over') if dir is 'next'
+      @$results.find('li:last').addClass('ac_over') if dir is 'prev'
+      return
+    $next.addClass('ac_over')
+  
+  select: (e) =>
+    index = $(e.target).index()
+    @trigger 'select', @res[index]
+  
+  events:
+    'keyup': 'onKeyUp'
+    
+  onKeyUp: (e) =>
+    { up, down, enter } = up: 38, down: 40, enter: 13
+    switch e.which
+      when up then @over 'prev'
+      when down then @over 'next'
+      when enter then @select target: @$results.find('.ac_over')
+      else @search()
--- public/assets/app/client/views/shared/autocomplete.js
+++ public/assets/app/client/views/shared/autocomplete.js
@@ -8,19 +8,40 @@
     __extends(AutocompleteView, _super);
 
     function AutocompleteView() {
-      this.resToHTML = __bind(this.resToHTML, this);
+      this.onKeyUp = __bind(this.onKeyUp, this);
+
+      this.select = __bind(this.select, this);
 
       this.search = __bind(this.search, this);
       return AutocompleteView.__super__.constructor.apply(this, arguments);
     }
 
-    AutocompleteView.prototype.initialize = function() {
-      this.search = _.debounce(this.search, 100);
-      return this.$results = this.$el.after("<div class='results'></div>").next('.results');
+    AutocompleteView.prototype.defaults = {
+      field: 'name',
+      delay: 300
     };
 
-    AutocompleteView.prototype.events = {
-      'keyup': 'search'
+    AutocompleteView.prototype.initialize = function() {
+      this.options = _.extend(this.defaults, this.options);
+      this.search = _.debounce(this.search, this.options.delay);
+      return this.$results = this.$el.after("<div class='ac_results'></div>").next('.ac_results');
+    };
+
+    AutocompleteView.prototype.renderLi = function(obj) {
+      return "<li>" + obj[this.options.field] + "</li>";
+    };
+
+    AutocompleteView.prototype.renderResults = function(arr) {
+      var obj;
+      return "<ul>" + (((function() {
+        var _i, _len, _results;
+        _results = [];
+        for (_i = 0, _len = arr.length; _i < _len; _i++) {
+          obj = arr[_i];
+          _results.push(this.renderLi(obj));
+        }
+        return _results;
+      }).call(this)).join('')) + "</ul>";
     };
 
     AutocompleteView.prototype.search = function() {
@@ -31,21 +52,59 @@
           term: this.$el.val()
         }
       }).then(function(res) {
-        return _this.$results.html(_this.resToHTML(res));
+        _this.res = res;
+        _this.$results.html(_this.renderResults(_this.res));
+        return _this.$results.find('li').click(_this.select);
       });
     };
 
-    AutocompleteView.prototype.resToHTML = function(res) {
-      var obj;
-      return "<ul>\n  " + (((function() {
-        var _i, _len, _results;
-        _results = [];
-        for (_i = 0, _len = res.length; _i < _len; _i++) {
-          obj = res[_i];
-          _results.push("<li>" + obj.name + "</li>");
+    AutocompleteView.prototype.over = function(dir) {
+      var $next, $over;
+      $over = this.$results.find('.ac_over');
+      $over.parent().children().removeClass('ac_over');
+      $next = $over[dir]();
+      if (!$next.length) {
+        if (dir === 'next') {
+          this.$results.find('li:first').addClass('ac_over');
         }
-        return _results;
-      })()).join('')) + "\n</ul>";
+        if (dir === 'prev') {
+          this.$results.find('li:last').addClass('ac_over');
+        }
+        return;
+      }
+      return $next.addClass('ac_over');
+    };
+
+    AutocompleteView.prototype.select = function(e) {
+      var index;
+      index = $(e.target).index();
+      this.trigger('select', this.res[index]);
+      return console.log(this.res[index]);
+    };
+
+    AutocompleteView.prototype.events = {
+      'keyup': 'onKeyUp'
+    };
+
+    AutocompleteView.prototype.onKeyUp = function(e) {
+      var down, enter, up, _ref;
+      _ref = {
+        up: 38,
+        down: 40,
+        enter: 13
+      }, up = _ref.up, down = _ref.down, enter = _ref.enter;
+      switch (e.which) {
+        case up:
+          return this.over('prev');
+        case down:
+          return this.over('next');
+        case enter:
+          return this.select({
+            target: this.$results.find('.ac_over')
+          });
+        default:
+          return this.search();
+      }
     };
 
     return AutocompleteView;
